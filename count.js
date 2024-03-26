662b368d5f40e72184d24c02d2ce39616e7bc8e1
gideon rotich <gideonrotich2001@gmail.com>
2024-03-26 22:54:54 +0300

    count

---
+++ count.js
@@ -0,0 +1,21 @@
+
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase");
+const countlable = document.getElementById("countlable");
+
+let count = 0;
+
+increase.onclick = function(){
+    count++;
+    countlable.textContent = count;
+}
+
+decrease.onclick = function(){
+    count--;
+    countlable.textContent = count;
+}
+reset.onclick = function(){
+    count = 0;
+    countlable.textContent = count;
+}
